ce0ef86d351b1290bfc3d301167598f93afb73cb
Oscar Larios <javier.atoaxaca@gmail.com>
2021-10-01 12:16:33 -0500

    Se creo los archivos de list_page y save_page de dart, se creo los cuadros de texto y validaciones en el archivo de save_page.dart.

--- lib/pages/save_page.dart
+++ lib/pages/save_page.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
 
-
 class SavePage extends StatelessWidget {
-
   static const String ROUTE = "/save";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar:AppBar(title: Text("Save"),),
-    body: Container(child: Text("Pagina de Guardar"),) ,);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Guardar"),
+      ),
+      body: Container(
+        child: _FormSave(),
+      ),
+    );
+  }
+}
+
+class _FormSave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: "Titulo",
+                  border:
+                      OutlineInputBorder() //borderRadius: BorderRadius.all(Radius.circular(50))
+                  ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextFormField(
+              maxLines: 8,
+              maxLength: 1000,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Tienes que colocar Datos";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  labelText: "Contenido",
+                  border:
+                      OutlineInputBorder() //borderRadius: BorderRadius.all(Radius.circular(50))
+                  ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
